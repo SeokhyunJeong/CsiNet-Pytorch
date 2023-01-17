7333f5ea03f59a3e4950a74a1d65d948d6885b0e
SeokhyunJeong <39179927+SeokhyunJeong@users.noreply.github.com>
2023-01-17 13:32:24 +0900

    20230117

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from train import train
 
 ########config#######
 
-epochs = 300
+epochs = 40
 learning_rate = 0.001
 lr_decay_freq = 20
 lr_decay = 0.9
@@ -19,20 +19,20 @@ device = 'cuda'
 
 def run(encoded_dim):
     trainer = train('./filepath/data_large.csv',
-                    epochs, encoded_dim, learning_rate, lr_decay_freq,  lr_decay, print_freq, device)
+                    epochs, encoded_dim, learning_rate, lr_decay_freq, lr_decay, print_freq, device)
     encoder, decoder = trainer.train_epoch()
     online_trainer1 = train('./filepath/data_online.csv',
-                           0, encoded_dim, learning_rate, lr_decay_freq, lr_decay, print_freq, device,
-                           online=True, encoder=encoder, decoder=decoder)
-    online_trainer1.train_online_epoch()
+                            0, encoded_dim, learning_rate, lr_decay_freq, lr_decay, print_freq, device,
+                            encoder=encoder, decoder=decoder)
+    online_trainer1.train_epoch()
     online_trainer2 = train('./filepath/data_online.csv',
-                           epochs, encoded_dim, learning_rate, lr_decay_freq, lr_decay, print_freq, device,
-                           online=True, encoder=encoder, decoder=decoder)
+                            epochs, encoded_dim, learning_rate, lr_decay_freq, lr_decay, print_freq, device,
+                            encoder=encoder, decoder=decoder)
     online_trainer2.train_online_epoch()
     online_trainer3 = train('./filepath/data_online.csv',
-                    epochs, encoded_dim, learning_rate, lr_decay_freq, lr_decay, print_freq, device)
-    online_trainer3.train_online_epoch()
+                            epochs, encoded_dim, learning_rate, lr_decay_freq, lr_decay, print_freq, device)
+    online_trainer3.train_epoch()
 
 
-dim = 32
+dim = 128
 run(encoded_dim=dim)
